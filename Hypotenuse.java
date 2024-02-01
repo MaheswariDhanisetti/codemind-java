03ce1e6f36ce19d3983265d4ede10d1422499c09
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-02-01 11:15:43 +0530

    import java.util.*; public class hyp{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextInt();         double b=sc.nextInt();         System.out.printf("%.2f",Math.sqrt((a*a)+(b*b)));     } }

---
+++ Hypotenuse.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class hyp{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextInt();
+        double b=sc.nextInt();
+        System.out.printf("%.2f",Math.sqrt((a*a)+(b*b)));
+    }
+}
