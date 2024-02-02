7f522addb814028bdb4e7894f9166283d752d9d1
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-02-02 16:16:04 +0530

    import java.util.*; public class profit{ public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     String s=sc.next();     int n=Integer.parseInt(s,8);
        System.out.print(n);
    }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class profit{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    String s=sc.next();
+    int n=Integer.parseInt(s,8);
+    
+    System.out.print(n);
+}
+}
