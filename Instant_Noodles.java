9e41d2a2ae9b2acc23f41e734777e4eb73e654af
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-02-02 16:03:28 +0530

    import java.util.*; public class profit{ public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     int a=sc.nextInt();     int b=sc.nextInt();     int ans= a*b;
        System.out.println(ans);
    }
    }

---
+++ Instant_Noodles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class profit{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int ans= a*b;
+    
+    System.out.println(ans);
+}
+}
