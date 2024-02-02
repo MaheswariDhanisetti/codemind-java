afc9d2e31d31d555354112fd235c690dd2552a79
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-02-02 16:11:54 +0530

    import java.util.*; public class profit{ public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     double a=sc.nextInt();     double ans=(10*a)/36;
        System.out.printf("%.2f",ans);
    }
    }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class profit{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    double a=sc.nextInt();
+    double ans=(10*a)/36;
+    
+    System.out.printf("%.2f",ans);
+}
+}
