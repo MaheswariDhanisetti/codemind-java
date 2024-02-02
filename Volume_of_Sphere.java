81aa6579cdeb01580e27e66942c5e5abc0231f3b
Maheswari Dhanisetti <119394667+MaheswariDhanisetti@users.noreply.github.com>
2024-02-02 15:57:52 +0530

    import java.util.*; public class profit{ public static void main(String[] args) {     Scanner sc=new Scanner(System.in);     double a=sc.nextInt();     double v=(4*3.14*a*a*a)/3;
        System.out.printf("%.2f",v);
    }
    }

---
+++ Volume_of_Sphere.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class profit{
+public static void main(String[] args)
+{
+    Scanner sc=new Scanner(System.in);
+    double a=sc.nextInt();
+    double v=(4*3.14*a*a*a)/3;
+    
+    System.out.printf("%.2f",v);
+}
+}
